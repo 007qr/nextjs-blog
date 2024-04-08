4e9824f1e4b06c93793cfc2b5c6b709a5fdc81e8
007qr <aayushpatil558321@gmail.com>
2024-04-08 23:53:26 +0530

    disabled ai

--- src/novel-editor/generative/ai-selector.tsx
+++ src/novel-editor/generative/ai-selector.tsx
@@ -42,79 +42,86 @@ export function AISelector({ open, onOpenChange }: AISelectorProps) {
   const hasCompletion = completion.length > 0;
 
   return (
-    <Command className="w-[350px]">
-      {hasCompletion && (
-        <div className="flex max-h-[400px]">
-          <ScrollArea>
-            <div className="prose p-2 px-4 prose-sm">
-              <Markdown>{completion}</Markdown>
-            </div>
-          </ScrollArea>
-        </div>
-      )}
-
-      {isLoading && (
-        <div className="flex h-12 w-full items-center px-4 text-sm font-medium text-muted-foreground text-purple-500">
+<div className="flex h-12 w-full items-center px-4 text-sm font-medium text-muted-foreground text-purple-500">
           <Magic className="mr-2 h-4 w-4 shrink-0  " />
           AI is thinking
           <div className="ml-2 mt-1">
             <CrazySpinner />
           </div>
         </div>
-      )}
-      {!isLoading && (
-        <>
-          <div className="relative">
-            <CommandInput
-              value={inputValue}
-              onValueChange={setInputValue}
-              autoFocus
-              placeholder={
-                hasCompletion
-                  ? "Tell AI what to do next"
-                  : "Ask AI to edit or generate..."
-              }
-              onFocus={() => addAIHighlight(editor)}
-            />
-            <Button
-              size="icon"
-              className="absolute right-2 top-1/2 h-6 w-6 -translate-y-1/2 rounded-full bg-purple-500 hover:bg-purple-900"
-              onClick={() => {
-                if (completion)
-                  return complete(completion, {
-                    body: { option: "zap", command: inputValue },
-                  }).then(() => setInputValue(""));
+    // <Command className="w-[350px]">
+    //   {hasCompletion && (
+    //     <div className="flex max-h-[400px]">
+    //       <ScrollArea>
+    //         <div className="prose p-2 px-4 prose-sm">
+    //           <Markdown>{completion}</Markdown>
+    //         </div>
+    //       </ScrollArea>
+    //     </div>
+    //   )}
 
-                const slice = editor.state.selection.content();
-                const text = editor.storage.markdown.serializer.serialize(
-                  slice.content,
-                );
+    //   {isLoading && (
+    //     <div className="flex h-12 w-full items-center px-4 text-sm font-medium text-muted-foreground text-purple-500">
+    //       <Magic className="mr-2 h-4 w-4 shrink-0  " />
+    //       AI is thinking
+    //       <div className="ml-2 mt-1">
+    //         <CrazySpinner />
+    //       </div>
+    //     </div>
+    //   )}
+    //   {!isLoading && (
+    //     <>
+    //       <div className="relative">
+    //         <CommandInput
+    //           value={inputValue}
+    //           onValueChange={setInputValue}
+    //           autoFocus
+    //           placeholder={
+    //             hasCompletion
+    //               ? "Tell AI what to do next"
+    //               : "Ask AI to edit or generate..."
+    //           }
+    //           onFocus={() => addAIHighlight(editor)}
+    //         />
+    //         <Button
+    //           size="icon"
+    //           className="absolute right-2 top-1/2 h-6 w-6 -translate-y-1/2 rounded-full bg-purple-500 hover:bg-purple-900"
+    //           onClick={() => {
+    //             if (completion)
+    //               return complete(completion, {
+    //                 body: { option: "zap", command: inputValue },
+    //               }).then(() => setInputValue(""));
 
-                complete(text, {
-                  body: { option: "zap", command: inputValue },
-                }).then(() => setInputValue(""));
-              }}
-            >
-              <ArrowUp className="h-4 w-4" />
-            </Button>
-          </div>
-          {hasCompletion ? (
-            <AICompletionCommands
-              onDiscard={() => {
-                editor.chain().unsetHighlight().focus().run();
-                onOpenChange(false);
-              }}
-              completion={completion}
-            />
-          ) : (
-            <AISelectorCommands
-              onSelect={(value, option) =>
-                complete(value, { body: { option } })
-              }
-            />
-          )}
-        </>
-      )}
-    </Command>
+    //             const slice = editor.state.selection.content();
+    //             const text = editor.storage.markdown.serializer.serialize(
+    //               slice.content,
+    //             );
+
+    //             complete(text, {
+    //               body: { option: "zap", command: inputValue },
+    //             }).then(() => setInputValue(""));
+    //           }}
+    //         >
+    //           <ArrowUp className="h-4 w-4" />
+    //         </Button>
+    //       </div>
+    //       {hasCompletion ? (
+    //         <AICompletionCommands
+    //           onDiscard={() => {
+    //             editor.chain().unsetHighlight().focus().run();
+    //             onOpenChange(false);
+    //           }}
+    //           completion={completion}
+    //         />
+    //       ) : (
+    //         <AISelectorCommands
+    //           onSelect={(value, option) =>
+    //             complete(value, { body: { option } })
+    //           }
+    //         />
+    //       )}
+    //     </>
+    //   )}
+    // </Command>
   );
 }
